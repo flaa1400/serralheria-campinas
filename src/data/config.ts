a78f494d9744c9064c8fbcf27e3d081b4f0d9855
flaa1400 <fla141989@gmail.com>
2026-07-28 19:13:28 -0300

    feat: upload src/data/config.ts

---
+++ src/data/config.ts
@@ -0,0 +1,43 @@
+export const SITE_CONFIG = {
+  site: "https://serralheriaprecisaometal.com.br",
+  name: "Serralheria Precisão Metal",
+  legalName: "Serralheria Precisão Metal Campinas",
+  titleTemplate: "%s | Serralheria Precisão Metal - Campinas",
+  defaultTitle: "Serralheria em Campinas | Serralheria Precisão Metal",
+  description: "Serralheria em Campinas de alto padrão. Especialistas em portões sob medida, estruturas metálicas, mezaninos, grades, corrimãos e soldas industriais com garantia de qualidade.",
+
+  address: {
+    street: "Av. John Boyd Dunlop, 3520",
+    neighborhood: "Jardim Nova Esperança",
+    city: "Campinas",
+    state: "SP",
+    zipCode: "13060-803",
+    country: "BR",
+    full: "Av. John Boyd Dunlop, 3520 - Jardim Nova Esperança, Campinas - SP"
+  },
+
+  phone: "(11) 96429-9548",
+  whatsappNumber: "5511964299548",
+  whatsappFormatted: "(11) 96429-9548",
+  whatsappUrl: "https://wa.me/5511964299548?text=Ol%C3%A1%20vim%20pelo%20Site%20Precis%C3%A3o%20Metal%20e%20gostaria%20de%20fazer%20um%20Or%C3%A7amento",
+  email: "serralheriaprecisaometal@yahoo.com",
+
+  geo: {
+    latitude: -22.9248,
+    longitude: -47.1147
+  },
+
+  openingHours: [
+    "Mo-Fr 07:30-18:00",
+    "Sa 08:00-12:00"
+  ],
+
+  priceRange: "$$",
+
+  social: {
+    instagram: "https://www.instagram.com/precisaometal?igsh=MTR3dWVyaTg4cHd4OA==",
+    facebook: "https://www.facebook.com/profile.php?id=61592512594259"
+  },
+
+  ogImage: "/og-image.jpg"
+};
